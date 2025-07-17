b14a304d5d90a01bd87a2310ba707842410d8472
doomguy504 <97200707+Doomguy504@users.noreply.github.com>
2025-07-17 17:18:43 +0000

    make squares change color on mouse enter

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const gridContainer = document.querySelector('#grid-container');
 const GRID_WIDTH = 16;
 const GRID_HEIGHT = 16;
 
+function changeColor(e){
+    e.target.style.backgroundColor = 'black';
+}
+
 for (let gridColumnIndex = 0; gridColumnIndex < GRID_HEIGHT; gridColumnIndex++){
     const gridRow = document.createElement('div');
     gridContainer.appendChild(gridRow);
@@ -10,7 +14,9 @@ for (let gridColumnIndex = 0; gridColumnIndex < GRID_HEIGHT; gridColumnIndex++){
     for (let gridRowIndex = 0; gridRowIndex < GRID_WIDTH; gridRowIndex++){
         const square = document.createElement('div');
         square.className = 'square';
+        square.addEventListener('mouseenter', changeColor);
 
         gridRow.appendChild(square);
     }
 }
+
