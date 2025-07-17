7d520a095de9c62f2cb2e06d17f156d7b0b226ae
doomguy504 <97200707+Doomguy504@users.noreply.github.com>
2025-07-17 18:40:48 +0000

    add functionality to size change button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector('#grid-container');
+const sizeButton = document.querySelector('#size-button');
 const resetButton = document.querySelector('#reset-button');
 
 const INITIAL_GRID_SIZE = 16;
@@ -13,6 +14,19 @@ function reset(){
     squares.forEach((e) => {
         e.style.backgroundColor = 'white';
     });
+}
+
+function changeGridSize(){
+    let newGridSize = prompt('Enter new grid size', '16');
+    newGridSize = parseInt(newGridSize);
+
+    while (!newGridSize ||  newGridSize < 0 || newGridSize > 100){
+        newGridSize = prompt('Invalid input. Must be between 0 and 100', '16');
+        newGridSize = parseInt(newGridSize);
+    }
+
+    gridContainer.replaceChildren();
+    createGrid(newGridSize);
 }
 
 function createGrid(gridDimensions){
@@ -32,5 +46,6 @@ function createGrid(gridDimensions){
 }
 
 resetButton.addEventListener('click', reset);
+sizeButton.addEventListener('click', changeGridSize);
 
 createGrid(INITIAL_GRID_SIZE);
